eecf7c8bbcc714e7e4403d94305eca29bbe32dd0
Diego Carvalho <157425806+diegocrvo@users.noreply.github.com>
2025-03-18 23:29:15 -0300

    feat: exercise

---
+++ JavaScript/Exercicios/sentenca_e_bloco.js
@@ -0,0 +1,13 @@
+console.log("Bloco 1")
+for (let i = 1; i <= 10; i ++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+console.log("Bloco 2")
+for (let i = 1; i <= 10; i ++) {
+    if(i % 2 == 1) {
+        console.log(i)
+    }
+}
